b1447a738a9446907461e21738a4659f3d5a3c57
yongkim <omiyong@gmail.com>
2025-07-08 16:14:44 +0900

    [KY] 25.07.08 [최초커밋]

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    /*----------------------------
+        브라우저에서 "http://localhost:8080/hello" 로 접속시
+        브라우저에서 "Hello, Spring!" 가 출력됨을 확인 !
+    ----------------------------*/
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, Spring!";
+    }
+}
